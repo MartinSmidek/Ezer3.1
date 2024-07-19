0389beb49ad8c8392786b07b92fed4a015fbfc8b
Martin Šmídek <martin@smidek.eu>
2024-07-19 17:09:56 +0200

    * fce explode_csv
    * query_track už nepoužívá ezer_qry

--- server/ae_slib.php
+++ server/ae_slib.php
@@ -696,7 +696,9 @@ function query($qry,$db='.main.') {
 #   INSERT INTO tab (f1,f2,...) VALUES (v1,v2,...) 
 #   UPDATE tab SET f1=v1, f2=v2, ... WHERE id_tab=v0
 # kde vi jsou jednoduché hodnoty: číslo nebo string uzavřený v apostorfech 
-function query_track($qry) {
+# trasovaná tabulka musí být uvedena v $mysql_tracked, jeji klíč musí být buďto ve tvaru id_tab
+# nebo být uveden v $mysql_tracked_id jako tab=>id
+function query_track($qry,$db='.main.') {
   global $mysql_db_track, $mysql_tracked, $mysql_tracked_id;
   // rozklad výrazu: 1:table, 2:field list, 3:values list
   $res= 0;
@@ -709,15 +711,21 @@ function query_track($qry) {
   $fce= $m[1] ?: '';
   $tab= $m[2] ?: '';
   if ( $mysql_db_track && strpos($mysql_tracked,",$tab,")!==false ) {
+    global $USER;
+    $abbr= isset($USER->abbr) ? $USER->abbr : 'WEB';
     if ($ok && $fce=='INSERT') {
       $fld= explode_csv($m[3]); 
       $val= explode_csv($m[4]); 
-      $chng= [];
+      $res= query($qry,$db);
+      $key_val= pdo_insert_id();
       for ($i= 0; $i<count($fld); $i++) {
-        $v= trim($val[$i],"'");
-        $chng[]= (object)['fld'=>$fld[$i],'op'=>'i','val'=>$v];
+        $f= $fld[$i];
+        $v= $val[$i];
+//        if ($v[0]=="'") $v= substr($v,1,-1);
+//        $v= pdo_real_escape_string($v);
+        pdo_query("INSERT INTO _track (kdy,kdo,kde,klic,op,fld,val) "
+            . "VALUE (NOW(),'$abbr','$tab',$key_val,'i','$f',$v)",null,null,false,$db);
       }
-      $res= ezer_qry("INSERT",$tab,0,$chng);
     }
     elseif ($ok && $fce=='UPDATE') {
   //    debug($m);
@@ -727,17 +735,16 @@ function query_track($qry) {
       // kontrola podmínky
       $ok= $key_id=="id_$tab" || $key_id==$mysql_tracked_id[$tab];
       if ($ok) {
-        global $USER;
         foreach ($sets as $set) {
           list($fld,$val)= explode('=',$set,2);
           $old= select($fld,$tab,"$key_id=$key_val");
           $old= pdo_real_escape_string($old);
           if ($val[0]=="'") $val= substr($val,1,-1);
           $val= pdo_real_escape_string($val);
-          query("INSERT INTO _track (kdy,kdo,kde,klic,op,fld,old,val) "
-              . "VALUE (NOW(),'$USER->abbr','$tab',$key_val,'u','$fld','$old','$val')");
+          pdo_query("INSERT INTO _track (kdy,kdo,kde,klic,op,fld,old,val) "
+            . "VALUE (NOW(),'$abbr','$tab',$key_val,'u','$fld','$old','$val')",null,null,false,$db);
         }
-        $res= query($qry);
+        $res= query($qry,$db);
       }
     }
     elseif ($ok && $fce=='DELETE') {
@@ -746,10 +753,9 @@ function query_track($qry) {
       // kontrola podmínky
       $ok= $key_id=="id_$tab" || $key_id==$mysql_tracked_id[$tab];
       if ($ok) {
-        global $USER;
-        query("INSERT INTO _track (kdy,kdo,kde,klic,op) "
-            . "VALUE (NOW(),'$USER->abbr','$tab',$key_val,'x')");
-        $res= query("DELETE FROM $tab WHERE $key_id=$key_val");
+        pdo_query("INSERT INTO _track (kdy,kdo,kde,klic,op) "
+            . "VALUE (NOW(),'$abbr','$tab',$key_val,'x')",null,null,false,$db);
+        $res= query("DELETE FROM $tab WHERE $key_id=$key_val",$db);
       }
     }
     else {
@@ -760,7 +766,7 @@ function query_track($qry) {
     }
   }
   else {
-    $res= query($qry);
+    $res= query($qry,$db);
   }
 end:
   return $res;
@@ -916,6 +922,63 @@ function ezer_json_encode($ao) {
     $js= is_numeric($ao) ? $ao : '"'.pdo_real_escape_string($ao).'"';
   }
   return $js;
+}
+# -------------------------------------------------------------------------------------- explode csv
+# split CSV s ohledem na závorky a apostrofy
+function explode_csv($str, $separator=",", $leftbracket="(", $rightbracket=")", $quote="'", $ignore_escaped_quotes=true ) {
+  $buffer = '';
+  $stack = array();
+  $depth = 0;
+  $char= '';
+  $betweenquotes = false;
+  $len = strlen($str);
+  for ($i=0; $i<$len; $i++) {
+    $previouschar = $char;
+    $char = $str[$i];
+    switch ($char) {
+      case $separator:
+        if (!$betweenquotes) {
+          if (!$depth) {
+            if ($buffer !== '') {
+              $stack[] = $buffer;
+              $buffer = '';
+            }
+            continue 2;
+          }
+        }
+        break;
+      case $quote:
+        if ($ignore_escaped_quotes) {
+          if ($previouschar!="\\") {
+            $betweenquotes = !$betweenquotes;
+          }
+        } else {
+          $betweenquotes = !$betweenquotes;
+        }
+        break;
+      case $leftbracket:
+        if (!$betweenquotes) {
+          $depth++;
+        }
+        break;
+      case $rightbracket:
+        if (!$betweenquotes) {
+          if ($depth) {
+            $depth--;
+          } else {
+            $stack[] = $buffer.$char;
+            $buffer = '';
+            continue 2;
+          }
+        }
+        break;
+      }
+      $buffer .= $char;
+  }
+  if ($buffer !== '') {
+    $stack[] = $buffer;
+  }
+  return $stack;
 }
 # ------------------------------------------------------------------------------------- emailIsValid
 # tells you if an email is in the correct form or not
